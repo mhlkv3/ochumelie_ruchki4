74043d70b06b3f5e08ad589b4eed99bc325db7df
bymhlkv <by.mhlkv@gmail.com>
2021-06-08 15:26:55 +0300

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	$(".menu_text.1").click(function(){
-		$(".ad_menu.hidden").toggleClass("hidden");
+		$(".ad_menu_1.hidden").toggleClass("hidden");
+	});
+});
+
+$(document).ready(function(){
+	$(".menu_text.2").click(function(){
+		$(".ad_menu_2.hidden").toggleClass("hidden");
 	});
 });
