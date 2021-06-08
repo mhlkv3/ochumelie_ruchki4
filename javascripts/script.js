4dbb58255f95e22d888bc9dbf238f0216f8b9762
bymhlkv <by.mhlkv@gmail.com>
2021-06-08 17:31:54 +0300

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$(".menu_text.2").click(function(){
-		$(".ad_menu_2.hidden").toggleClass("hidden");
+	$(".menu_text").click(function(){
+		$(".ad_menu_3.hidden,.ad_menu_4.hidden,.ad_menu_5.hidden").toggleClass("hidden");
 	});
 });
