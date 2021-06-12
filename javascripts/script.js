24ff5dd3a8f675885c05dea2939290f5427bfdc4
bymhlkv <by.mhlkv@gmail.com>
2021-06-12 23:17:44 +0300

    6

--- javascripts/script.js
+++ javascripts/script.js
@@ -9,3 +9,50 @@ $(document).ready(function(){
 		$(".ad_menu_3.hidden,.ad_menu_4.hidden,.ad_menu_5.hidden").toggleClass("hidden");
 	});
 });
+
+$(document).ready(function(){
+	$(".floors_txt_1").click(function(){
+		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
+		$(".reception_images").toggleClass("hidden");
+		$(".floors_txt_2, .floors_txt_3, .floors_txt_4, .floors_txt_5").css("color","#575757");
+		$(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").css("border-color","#575757");
+	});
+});
+
+
+
+$(document).ready(function(){
+	$(".floors_txt_2").click(function(){
+		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
+		$(".cafe_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").css("color","#575757");
+		$(".space_page_hr_1, .space_page_hr_3, .space_page_hr_4").css("border-color","#575757");
+	});
+});
+
+$(document).ready(function(){
+	$(".floors_txt_3").click(function(){
+		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
+		$(".stitching_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_4, .floors_txt_5").css("color","#575757");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_4").css("border-color","#575757");
+	});
+});
+
+$(document).ready(function(){
+	$(".floors_txt_4").click(function(){
+		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
+		$(".weaving_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_5").css("color","#575757");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3").css("border-color","#575757");
+	});
+});
+
+$(document).ready(function(){
+	$(".floors_txt_5").click(function(){
+		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
+		$(".knitting_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_4").css("color","#575757");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").css("border-color","#575757");
+	});
+});
