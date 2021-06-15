dcb6f2934225f2db4621ea58359a9dc03647b14b
bymhlkv <by.mhlkv@gmail.com>
2021-06-15 13:00:38 +0300

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,27 +1,56 @@
+/////////ГЛАВНАЯ СТРАНИЦА////////
+
+
 $(document).ready(function(){
 	$(".menu_text.1").click(function(){
 		$(".ad_menu_1.hidden,.ad_menu_2.hidden").toggleClass("hidden");
 	});
-});
 
-$(document).ready(function(){
 	$(".menu_text.2").click(function(){
 		$(".ad_menu_3.hidden,.ad_menu_4.hidden,.ad_menu_5.hidden").toggleClass("hidden");
 	});
-});
 
-$(document).ready(function(){
 	$(".ad_menu_4").click(function(){
 		$(".ad_menu_6.hidden,.ad_menu_7.hidden").toggleClass("hidden");
 	});
 });
 
+///////ФОТО ПРОСТРАНСТВА////////
+
 $(document).ready(function(){
 	$(".floors_txt_1").click(function(){
-		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
+		$(".space_images").toggleClass("hidden");
 		$(".reception_images").toggleClass("hidden");
-		$(".floors_txt_2, .floors_txt_3, .floors_txt_4, .floors_txt_5").css("color","#575757");
-		$(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").css("border-color","#575757");
+		$(".floors_txt_2, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+		$(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
+	});
+
+	$(".floors_txt_2").click(function(){
+		$(".space_images").toggleClass("hidden");
+		$(".cafe_images").removeClass("hidden");
+		$(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color");
+	});
+
+	$(".floors_txt_3").click(function(){
+		$(".space_images, .stitching_images").toggleClass("hidden");
+		$(".stitching_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_4").toggleClass("grey_color");
+	});
+
+	$(".floors_txt_4").click(function(){
+		$(".space_images, .weaving_images").toggleClass("hidden");
+		$(".weaving_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_5").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3").toggleClass("grey_color");
+	});
+
+	$(".floors_txt_5").click(function(){
+		$(".space_images, .knitting_images").toggleClass("hidden");
+		$(".knitting_images").toggleClass("hidden");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_4").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color");
 	});
 });
 
@@ -31,8 +60,8 @@ $(document).ready(function(){
 	$(".floors_txt_2").click(function(){
 		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
 		$(".cafe_images").toggleClass("hidden");
-		$(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").css("color","#575757");
-		$(".space_page_hr_1, .space_page_hr_3, .space_page_hr_4").css("border-color","#575757");
+		$(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color");
 	});
 });
 
@@ -40,8 +69,8 @@ $(document).ready(function(){
 	$(".floors_txt_3").click(function(){
 		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
 		$(".stitching_images").toggleClass("hidden");
-		$(".floors_txt_1, .floors_txt_2, .floors_txt_4, .floors_txt_5").css("color","#575757");
-		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_4").css("border-color","#575757");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_4").toggleClass("grey_color");
 	});
 });
 
@@ -49,8 +78,8 @@ $(document).ready(function(){
 	$(".floors_txt_4").click(function(){
 		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
 		$(".weaving_images").toggleClass("hidden");
-		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_5").css("color","#575757");
-		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3").css("border-color","#575757");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_5").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3").toggleClass("grey_color");
 	});
 });
 
@@ -58,8 +87,8 @@ $(document).ready(function(){
 	$(".floors_txt_5").click(function(){
 		$(".space_img1, .space_img2, .space_img3").toggleClass("hidden");
 		$(".knitting_images").toggleClass("hidden");
-		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_4").css("color","#575757");
-		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").css("border-color","#575757");
+		$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_4").toggleClass("grey_color");
+		$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color");
 	});
 });
 
