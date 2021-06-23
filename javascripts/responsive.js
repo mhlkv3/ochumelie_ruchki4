a91f6c55034324b26eaab3ae62010753607383f0
bymhlkv <by.mhlkv@gmail.com>
2021-06-23 17:25:33 +0300

    Update responsive.js

--- javascripts/responsive.js
+++ javascripts/responsive.js
@@ -1,14 +1,9 @@
 // $(document).ready(function(){
-//   if ($(window).width() > 375) {
-//     $(".floors_txt_1").click(function(){
-//       $(".space_images").toggleClass("hidden");
-//       $(".reception_images").toggleClass("hidden");
-//       $(".floors_txt_2, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
-//       $(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
-//     });
+//   if (!$(".floors_txt_2").hasClass('grey_color')) {
+//     $(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass('grey_color');
+//     $(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
 // } else {
-//   $(".floors_txt_1").click(function(){
-//     $(".floors_txt_1").addClass("color", "red");
-// });
+//     $(".floors_txt_2").removeClass('grey_color$(document).ready(function(){
+//       ');
 // }
 // });
