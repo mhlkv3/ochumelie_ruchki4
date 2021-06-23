00d1a6171df9741d02f0e1b0b505a1efc88ee19a
bymhlkv <by.mhlkv@gmail.com>
2021-06-23 21:58:39 +0300

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $("#loader").css("opacity","0");
-  }, 2500);
-  setTimeout(function(){
-    $("#loader").css("display","none");
-  }, 3500);
+// $(document).ready(function(){
+//   setTimeout(function(){
+//     $("#loader").css("opacity","0");
+//   }, 2500);
+//   setTimeout(function(){
+//     $("#loader").css("display","none");
+//   }, 3500);
 
 
 /////ГЛАВНЫЙ ЭКРАН//////
@@ -25,41 +25,42 @@ $(document).ready(function(){
 
 /////ФОТО ПРОСТРАНСТВА//////
 
-		$(".floors_txt_1").click(function(){
-			$(".space_images").addClass("hidden");
-			$(".reception_images").toggleClass("hidden");
-			$(".floors_txt_2, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
-			$(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
-		});
+$(".floors_txt_1").click(function(){
+	$(".space_images").addClass("hidden");
+	$(".reception_images").toggleClass("hidden");
+	$(".floors_txt_2, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+	$(" .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
+});
 
-		$(".floors_txt_2").click(function(){
-			$(".space_images").addClass("hidden");
-			$(".cafe_images").toggleClass("hidden");
-			$(".reception_images, .stitching_images, .weaving_images, .knitting_images").addClass("hidden");
-			$(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
-			$(".space_page_hr_1, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
-		});
+$(".floors_txt_2").click(function(){
+	$(".space_images").addClass("hidden");
+	$(".cafe_images").toggleClass("hidden");
+	$(".reception_images, .stitching_images, .weaving_images, .knitting_images").addClass("hidden");
+	$(".floors_txt_1, .floors_txt_3, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+	$(".space_page_hr_1, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
+});
 
-		$(".floors_txt_3").click(function(){
-			$(".space_images").addClass("hidden");
-			$(".stitching_images").toggleClass("hidden");
-			$(".floors_txt_1, .floors_txt_2, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
-			$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_4").toggleClass("grey_color_1");
-		});
+$(".floors_txt_3").click(function(){
+	$(".space_images").addClass("hidden");
+	$(".stitching_images").toggleClass("hidden");
+	$(".floors_txt_1, .floors_txt_2, .floors_txt_4, .floors_txt_5").toggleClass("grey_color");
+	$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_4").toggleClass("grey_color_1");
+});
 
-		$(".floors_txt_4").click(function(){
-			$(".space_images").addClass("hidden");
-			$(".weaving_images").toggleClass("hidden");
-			$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_5").toggleClass("grey_color");
-			$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3").toggleClass("grey_color_1");
-		});
+$(".floors_txt_4").click(function(){
+	$(".space_images").addClass("hidden");
+	$(".weaving_images").toggleClass("hidden");
+	$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_5").toggleClass("grey_color");
+	$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3").toggleClass("grey_color_1");
+});
 
-		$(".floors_txt_5").click(function(){
-			$(".space_images").add("hidden");
-			$(".knitting_images").toggleClass("hidden");
-			$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_4").toggleClass("grey_color");
-			$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
-		});
+$(".floors_txt_5").click(function(){
+	$(".space_images").add("hidden");
+	$(".knitting_images").toggleClass("hidden");
+	$(".floors_txt_1, .floors_txt_2, .floors_txt_3, .floors_txt_4").toggleClass("grey_color");
+	$(".space_page_hr_1, .space_page_hr_2, .space_page_hr_3, .space_page_hr_4").toggleClass("grey_color_1");
+});
+
 
 		/////НАВИГАЦИЯ//////
 
